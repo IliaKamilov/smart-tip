af7f13002bf20e86378c2032d85e5553e571b8ac
IliaKamilov <iliakmlv@gmail.com>
2021-06-25 00:51:48 +0300

    employee form validation

--- src/components/EmployeeForm.tsx
+++ src/components/EmployeeForm.tsx
@@ -93,12 +93,9 @@ const EmployeeForm: React.FC<EmployeeFormProps> = ({ employee, confirmText, canc
 
         if (e.currentTarget.name === 'name') {
             const reg = new RegExp(/[א-ת]+$/gi)
-<<<<<<< HEAD
 
             if (!reg.test(e.currentTarget.value)) return handleError('שם חייב להיות באותיות עברית בלבד.')
 
-=======
->>>>>>> 503e8ff18faa7f5e0d7e1b63cb805cdd56eb59f3
             if (e.currentTarget.value.length > 20) return handleError('שם יכול להכיל 20 תווים בלבד.')
         }
 
