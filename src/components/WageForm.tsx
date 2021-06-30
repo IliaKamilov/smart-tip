dfb414379f62f5abe4fbdfd4fe8894b835a99306
IliaKamilov <iliakmlv@gmail.com>
2021-06-30 13:49:01 +0300

    stretch items

--- src/components/WageForm.tsx
+++ src/components/WageForm.tsx
@@ -78,7 +78,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     container: {
         display: 'flex',
         width: '100%',
-        alignItems: 'flex-start',
+        alignItems: 'stretch',
         justifyContent: 'space-between',
     },
     field: {
