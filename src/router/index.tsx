39d9272ec1269441bd142100bd91f9ef897ea8ce
IliaKamilov <iliakmlv@gmail.com>
2021-06-30 05:06:50 +0300

    Browser Router Support

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import App from '../App'
+import AboutView from './views/AboutView'
+import ContactView from './views/ContactView'
+import NotFoundView from './views/NotFoundView'
 
-const AppRouter: React.FC = ({ children }) => {
+const AppRouter: React.FC = () => {
     return (
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <Switch>
                 <Route exact path="/">
                     <App />
+                </Route>
+                <Route exact path="/about">
+                    <AboutView />
+                </Route>
+                <Route exact path="/contact">
+                    <ContactView />
+                </Route>
+                <Route status={404}>
+                    <NotFoundView />
                 </Route>
             </Switch>
         </Router>
